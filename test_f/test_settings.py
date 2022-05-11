0cd5896514edf740bca4e787ea5a82830102de9d
valeriyamavritskaya <valerymavritskaya@gmail.com>
2022-05-11 18:25:52 +0300

    commit before new changes

--- test_f/test_settings.py
+++ test_f/test_settings.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+import pytest
 from datetime import date, datetime
 
 
@@ -24,11 +25,12 @@ def test_marketing_calendar(driver):
     assert "5-4-4" in settings_page.marketing_calendar_dd.text
 
 
+@pytest.mark.skip(reason="not implemented")
 def test_calendar(driver):
     today = date.today()
     settings_page = SettingsPage(driver)
     settings_page.calendar_input_icon.click()
-    day_10 = driver.find_element(By.CLASS_NAME, "react-datepicker__day--010")
+    day_10 = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/div/div[3]/div[2]/div/div[1]/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[3]/div[1]")
     day_10.click()
     sleep(2)
     settings_page.store_details_save.click()
@@ -87,3 +89,25 @@ def test_add_team_member(driver):
     assert "qwe@qwe.com" not in settings_page.team_member_block.text
 
 
+@pytest.mark.skip(reason="not implemented")
+def test_reset_password(driver):
+    settings_page = SettingsPage(driver)
+    sleep(2)
+    link = driver.find_element(By.CLASS_NAME, "general-details__reset-psw")
+    link.click()
+    sleep(2)
+    email = driver.find_element(By.ID, "email")
+    email.send_keys("mvv3893qa@gmail.com")
+    submit = driver.find_element(By.CLASS_NAME, "reset-page__submit")
+    submit.click()
+    driver.get('http://www.gmail.com')
+    login_box = driver.find_element(By.ID, "identifierId")
+    login_box.click()
+    ActionChains(driver).send_keys('mvv3893qa@gmail.com').send_keys(Keys.ENTER).perform()
+    pw_box = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input")
+    pw_box.click()
+    ActionChains(driver).send_keys('mvv3893qapass').send_keys(Keys.ENTER).perform()
+    letter = driver.find_element(By.ID, ":2e")
+    letter.click()
+    link3 = driver.find_element(By.XPATH, "/html/body/div[7]/div[3]/div/div[2]/div[1]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/table/tr/td[1]/div[2]/div[2]/div/div[3]/div[1]/div/div/div/div/div[1]/div[2]/div[3]/div[3]/div/div[1]/table/tbody/tr/td/div/table/tbody/tr/td/table[2]/tbody/tr[2]/td/a")
+    link3.click()
